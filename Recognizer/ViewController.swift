684f6f3a9a4d089a25ba5e8b31ab2a6bdcbb0e78
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-09-30 21:33:38 +0300

    update UI and activate access to photo

--- Recognizer/ViewController.swift
+++ Recognizer/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// Don't forget in plist add Privacy - Camera Usage Description and Photo Library Usage
 // 1 add CoreML and Vision
 import CoreML
 import Vision
@@ -28,10 +29,33 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         // 4 activate delegate of UIImagePickerControllerDelegate
         imagePicker.delegate = self
+        // 5 to pick a source we want to get access (camera or photo library)
+        imagePicker.sourceType = .photoLibrary
+        // 6 restrict allow to edit pic
+        imagePicker.allowsEditing = false
+    }
+    
+    // 8 Tells the delegate that the user picked a still image or movie.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // 9 unwrap uncropped image selected by the use
+        guard let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        // 10 to show an image on the screen
+        cameraImageView.image = userPickedImage
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        // 7 camera is activated when button pressed
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
+    
     
 }
 
